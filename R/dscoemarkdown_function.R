dafbaf6c602809fb6f513b4c4ac348b4ba2cbf79
Nick Uhorchak <nick.uhorchak@sofcdo.com>
2020-05-29 14:30:14 -0400

    removed first forward slash to test for linux compatability

--- R/dscoemarkdown_function.R
+++ R/dscoemarkdown_function.R
@@ -1,26 +1,26 @@
 #' Run Markdown
 #'
-#' 
+#'
 #'
 #' @export
-#' 
-#' 
+#'
+#'
 
 dscoe <- function(logo=TRUE, disclaimer=TRUE) {
   system <- system.file('',package='dscoemarkdown')
-  
+
   if(disclaimer == TRUE){
-    footer <- paste(system,'/style/disclaimer.html',sep='')
+    footer <- paste(system,'style/disclaimer.html',sep='')
   } else if(disclaimer == FALSE){
-    footer <- paste(system,'/style/noDisclaimer.html',sep='')
+    footer <- paste(system,'style/noDisclaimer.html',sep='')
   }
-  
+
   if(logo == TRUE){
-    css <- paste(system,'/style/dscoemarkdownLogo.css',sep='')
+    css <- paste(system,'style/dscoemarkdownLogo.css',sep='')
   } else if(logo == FALSE){
-    css <- paste(system,'/style/dscoemarkdownNoLogo.css',sep='')
+    css <- paste(system,'style/dscoemarkdownNoLogo.css',sep='')
   }
-  
+
   rmarkdown::html_document(css=css, includes=rmarkdown::includes(after_body=footer))
 
 }
